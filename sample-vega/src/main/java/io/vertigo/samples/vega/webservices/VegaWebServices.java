ed2ce0c4ee6c2b978b2a47c132b4c29cd81e6039
phlppchrtn <pchretien55@gmail.com>
2016-01-12 15:58:37 +0100

    del classpath

--- sample-vega/src/main/java/io/vertigo/samples/vega/webservices/VegaWebServices.java
+++ sample-vega/src/main/java/io/vertigo/samples/vega/webservices/VegaWebServices.java
@@ -8,10 +8,9 @@ import io.vertigo.vega.webservice.stereotype.AnonymousAccessAllowed;
 import io.vertigo.vega.webservice.stereotype.GET;
 import io.vertigo.vega.webservice.stereotype.POST;
 import io.vertigo.vega.webservice.stereotype.PathPrefix;
-import io.vertigo.lang.Component;
 
 @PathPrefix("/vega")
-public class VegaWebServices implements Component, WebServices {
+public class VegaWebServices implements WebServices {
 
 	@AnonymousAccessAllowed
 	@GET("/")
@@ -24,17 +23,17 @@ public class VegaWebServices implements Component, WebServices {
 	public String getAccessToken() {
 		return "Token generated";
 	}
-	
+
 	@AccessTokenMandatory
 	@POST("/testToken")
 	public String testAccessToken() {
 		return "Token tested";
 	}
-	
+
 	@AccessTokenConsume
 	@POST("/invalidateToken")
 	public String consumeAccessToken() {
 		return "Token consumed";
 	}
-	
+
 }
