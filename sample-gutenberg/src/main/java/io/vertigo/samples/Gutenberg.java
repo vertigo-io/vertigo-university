467cdbfebcc65d72ae362009afeea7edd8e855af
durandx <xavier.durand@mageos.com>
2017-03-20 14:44:48 +0100

    Removing ScrabbleManager

--- sample-gutenberg/src/main/java/io/vertigo/samples/Gutenberg.java
+++ sample-gutenberg/src/main/java/io/vertigo/samples/Gutenberg.java
@@ -3,11 +3,8 @@ package io.vertigo.samples;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
 
 import io.vertigo.app.AutoCloseableApp;
-import io.vertigo.samples.components.scrabble.ScrabbleManager;
 import io.vertigo.samples.components.text.TextProcessorManager;
 import io.vertigo.samples.config.SampleConfigBuilder;
 
@@ -20,33 +17,11 @@ public class Gutenberg {
 		try (final AutoCloseableApp app = new AutoCloseableApp(new SampleConfigBuilder().build())) {
 
 			final TextProcessorManager textProcessor = app.getComponentSpace().resolve(TextProcessorManager.class);
-			final ScrabbleManager scrabbleManager = app.getComponentSpace().resolve(ScrabbleManager.class);
 
 			final Path path = Paths.get(ClassLoader.getSystemResource("maupassant.txt").toURI());
 
 			final long resultNbLignes = textProcessor.process(path);
 			System.out.println("Nombre de lignes = " + resultNbLignes);
-
-			final long resultNbMots = scrabbleManager.nombreDeMots(path);
-			System.out.println("Nombre de mots = " + resultNbMots);
-
-			final String motLePlusLong = scrabbleManager.motLePlusLong(path);
-			System.out.println("Mot le plus long = " + motLePlusLong);
-
-			final Map<String, Long> histo = scrabbleManager.histogram(path);
-			//System.out.println("Histogram = " + histo);
-
-			final Map<String, Long> histo2 = scrabbleManager.histogram2(path);
-			//System.out.println("Histogram2 = " + histo2);
-
-			final int scoreMotLePlusLong = scrabbleManager.scoreScrabble(motLePlusLong);
-			System.out.println("Score mot le plus long = " + scoreMotLePlusLong);
-
-			final int meilleurScoreScrabble = scrabbleManager.meilleurScoreScrabble(path);
-			System.out.println("Meilleur score scrabble = " + meilleurScoreScrabble);
-
-			final Map<Integer, List<String>> histoScore = scrabbleManager.histogramScore(path);
-			System.out.println("Histo score scrabble = " + histoScore);
 
 		}
 	}
