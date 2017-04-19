0b31fe7393ab06dec8ff65607388f636d7202faf
phlppchrtn <pchretien55@gmail.com>
2017-04-19 16:10:23 +0200

    rem

--- sample-dao-full/src/main/javagen/io/vertigo/samples/dao/domain/DtDefinitions.java
+++ sample-dao-full/src/main/javagen/io/vertigo/samples/dao/domain/DtDefinitions.java
@@ -195,12 +195,6 @@ public final class DtDefinitions implements Iterable<Class<?>> {
 			public Class<?> next() {
 				return it.next().getDtClass();
 			}
-
-			/** {@inheritDoc} */
-			@Override
-			public void remove() {
-				//unsupported
-			}
 		};
 	}
 }
