6e2c02bcadf646f61517f54ad3444576b144b67f
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2017-04-20 17:32:20 +0200

    [demo-orchestra] boot with h2

--- vertigo-demo-orchestra/makeH2Db.bat
+++ vertigo-demo-orchestra/makeH2Db.bat
@@ -1,5 +1,3 @@
-java -cp src/dev/lib/h2-1.3.176.jar org.h2.tools.RunScript -url jdbc:h2:database/demo -script src/main/sqlgen/crebas.sql
-
-java -cp src/dev/lib/h2-1.3.176.jar org.h2.tools.RunScript -url jdbc:h2:database/demo -script src/main/bdd/script/installation/init_data.sql 
+java -cp src/dev/lib/h2-1.4.193.jar org.h2.tools.RunScript -url jdbc:h2:d:\temp\database\demo-orchestra -script src/dev/scripts/orchestra_create_init.sql
 
 pause
