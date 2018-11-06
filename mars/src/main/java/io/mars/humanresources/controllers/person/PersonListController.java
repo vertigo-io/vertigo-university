24ce0e2baae6f0512558615ebca24125bd48de28
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2018-11-06 18:55:33 +0100

    [mars] added cards

--- mars/src/main/java/io/mars/humanresources/controllers/person/PersonListController.java
+++ mars/src/main/java/io/mars/humanresources/controllers/person/PersonListController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.mars.humanresources.domain.Person;
 import io.mars.humanresources.services.person.PersonServices;
@@ -19,12 +20,14 @@ import io.vertigo.ui.impl.springmvc.controller.AbstractVSpringMvcController;
 public class PersonListController extends AbstractVSpringMvcController {
 
 	private static final ViewContextKey<Person> persons = ViewContextKey.of("persons");
+	private static final ViewContextKey<String> listRenderer = ViewContextKey.of("listRenderer");
 
 	@Inject
 	private PersonServices personServices;
 
 	@GetMapping("/")
 	public void initContext(final ViewContext viewContext) {
+		viewContext.publishRef(listRenderer, "table");
 		final DtListState dtListState = new DtListState(200, 0, null, null);
 		viewContext.publishDtList(persons, personServices.getPersons(dtListState));
 	}
@@ -33,6 +36,11 @@ public class PersonListController extends AbstractVSpringMvcController {
 	public ViewContext sort(final ViewContext viewContext, final DtListState dtListState) {
 		viewContext.publishDtList(persons, personServices.getPersons(dtListState));
 		return viewContext;
+	}
+
+	@PostMapping("/_changeRenderer")
+	public void changeRenderer(final ViewContext viewContext, @RequestParam("renderer") final String renderer) {
+		viewContext.publishRef(listRenderer, renderer);
 	}
 
 }
