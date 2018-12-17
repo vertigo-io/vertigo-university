044e7eb43956a41d49554b043965c30d3d572f8a
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2018-12-17 18:57:19 +0100

    [mars] add init master data of ticket status

--- mars/src/main/java/io/mars/boot/DataBaseInitializer.java
+++ mars/src/main/java/io/mars/boot/DataBaseInitializer.java
@@ -58,7 +58,7 @@ public class DataBaseInitializer implements ComponentInitializer {
 
 	private void createDataBase() {
 		final SqlConnection connection = sqlDataBaseManager.getConnectionProvider(SqlDataBaseManager.MAIN_CONNECTION_PROVIDER_NAME).obtainConnection();
-		execCallableStatement(connection, sqlDataBaseManager, "drop all objects;");
+		//execCallableStatement(connection, sqlDataBaseManager, "drop all objects;");
 		execSqlScript(connection, "sqlgen/crebas.sql");
 		execSqlScript(connection, "sqlgen/init_masterdata_base_type.sql");
 		execSqlScript(connection, "sqlgen/init_masterdata_work_order_status.sql");
