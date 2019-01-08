3b6f376d51d5b7ee059be2f752a5589d820a3ea1
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2019-01-08 20:20:42 +0100

    [samples] fix conf

--- vertigo-samples/sample-aop-modules/02-sample-notifications-corrige/src/main/java/io/vertigo/notifications/config/NotificationSampleConfigBuilder.java
+++ vertigo-samples/sample-aop-modules/02-sample-notifications-corrige/src/main/java/io/vertigo/notifications/config/NotificationSampleConfigBuilder.java
@@ -5,9 +5,7 @@ import io.vertigo.app.config.ModuleConfig;
 import io.vertigo.commons.CommonsFeatures;
 import io.vertigo.core.param.Param;
 import io.vertigo.dynamo.DynamoFeatures;
-import io.vertigo.mail.MailManager;
-import io.vertigo.mail.impl.MailManagerImpl;
-import io.vertigo.mail.plugins.javax.JavaxSendMailPlugin;
+import io.vertigo.mail.MailFeatures;
 import io.vertigo.notifications.NotificationManager;
 import io.vertigo.notifications.aspects.supervision.SupervisionAspect;
 import io.vertigo.notifications.aspects.supervision.SupervisionManager;
@@ -28,34 +26,33 @@ import io.vertigo.notifications.plugins.twitter.TwitterNotificationPlugin;
 public class NotificationSampleConfigBuilder {
 
 	public AppConfig build() {
-		//@formatter:off
 		return AppConfig.builder()
 				.beginBoot()
 				.withLocales("fr")
 				.endBoot()
 				.addModule(new CommonsFeatures().build())
 				.addModule(new DynamoFeatures().build())
-				.addModule( ModuleConfig.builder("notificationAspects")
+				.addModule(ModuleConfig.builder("notificationAspects")
 						.addComponent(SupervisionManager.class, SupervisionManagerImpl.class)
 						.addComponent(TraceManager.class, TraceManagerImpl.class)
 						.addAspect(SupervisionAspect.class)
 						.addAspect(TraceAspect.class)
 						.build())
-				.addModule( ModuleConfig.builder("notifications")
+				.addModule(new MailFeatures()
+						.withJavax(
+								Param.of("storeProtocol", "smtp"),
+								Param.of("host", "localdelivery.klee.lan.net"),
+								Param.of("developmentMode", "true"),
+								Param.of("developmentMailTo", "prenom.nom@kleegroup.com"))
+						.build())
+				.addModule(ModuleConfig.builder("notifications")
 						.addComponent(NotificationManager.class, NotificationManagerImpl.class)
-							.addPlugin(IftttNotificationPlugin.class,
-									Param.of("proxyHost", "172.20.0.9"),
-									Param.of("proxyPort", "3128"))
-							.addPlugin(TwitterNotificationPlugin.class)
-							.addPlugin(MailNotificationPlugin.class)
-						.addComponent(MailManager.class, MailManagerImpl.class)
-							.addPlugin(JavaxSendMailPlugin.class,
-									Param.of("storeProtocol", "smtp"),
-									Param.of("host", "localdelivery.klee.lan.net"),
-									Param.of("developmentMode", "true"),
-									Param.of("developmentMailTo", "prenom.nom@kleegroup.com"))
-						.build()
-						)
+						.addPlugin(IftttNotificationPlugin.class,
+								Param.of("proxyHost", "172.20.0.9"),
+								Param.of("proxyPort", "3128"))
+						.addPlugin(TwitterNotificationPlugin.class)
+						.addPlugin(MailNotificationPlugin.class)
+						.build())
 				.build();
 	}
 
