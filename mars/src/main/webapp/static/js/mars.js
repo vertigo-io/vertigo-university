9d885d8be9a6ea7a8041ec39be466b105b56261e
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2019-01-23 19:44:11 +0100

    [mars] fix glitch in notifications

--- mars/src/main/webapp/static/js/mars.js
+++ mars/src/main/webapp/static/js/mars.js
@@ -2,8 +2,8 @@ VUiExtensions.methods.notificationTypes = function() {
 	return {
 		'MARS-LOGIN' : 'mdi-account-check',
 		'MARS-LOGIN-ATTEMP' : 'mdi-account-alert',
-		'MARS-TICKET-LEDGER' : 'mdi mdi-issue mdi-ethereum',
-		'MARS-MISSION-LEDGER' : 'mdi mdi-school mdi-ethereum',
+		'MARS-TICKET-LEDGER' : 'mdi-mdi-issue mdi-ethereum',
+		'MARS-MISSION-LEDGER' : 'mdi-mdi-school mdi-ethereum',
 		'MARS-BASE' : 'fas fa-dice-d20'
 	}
 }
