5f2eeb6b0b08eb5c9ad2e1ada9d4cba702ffe35b
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2019-04-26 15:44:53 +0200

    [demo-struts2] remove

--- vertigo-demo-struts2/makeH2Db.bat
+++
@@ -1,5 +0,0 @@
-java -cp src/dev/lib/h2-1.3.176.jar org.h2.tools.RunScript -url jdbc:h2:database/demo -script src/main/sqlgen/crebas.sql
-
-java -cp src/dev/lib/h2-1.3.176.jar org.h2.tools.RunScript -url jdbc:h2:database/demo -script src/main/bdd/script/installation/init_data.sql 
-
-pause
