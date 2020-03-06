f13208c65946e634f7362dad14239f8e7116cb32
phlppchrtn <philippe.chretien@kleegroup.com>
2020-03-06 18:05:51 +0100

    add datamodel on config.

--- sample-hello-world/src/main/java/io/vertigo/samples/hello/config/HelloConfigurator.java
+++ sample-hello-world/src/main/java/io/vertigo/samples/hello/config/HelloConfigurator.java
@@ -1,16 +1,16 @@
 package io.vertigo.samples.hello.config;
 
-import io.vertigo.commons.CommonsFeatures;
 import io.vertigo.core.node.config.ModuleConfig;
 import io.vertigo.core.node.config.NodeConfig;
 import io.vertigo.core.param.Param;
+import io.vertigo.datamodel.DataModelFeatures;
 import io.vertigo.samples.hello.webservices.HelloWebServices;
 import io.vertigo.vega.VegaFeatures;
 
 public final class HelloConfigurator {
 	public static NodeConfig config(final int port) {
 		return NodeConfig.builder()
-				.addModule(new CommonsFeatures().build())
+				.addModule(new DataModelFeatures().build())
 				.addModule(new VegaFeatures().withWebServices().withWebServicesEmbeddedServer(Param.of("port", Integer.toString(port))).build())
 				//-----Declaration of a module named 'Hello' which contains a webservice component.
 				.addModule(ModuleConfig.builder("Hello")
