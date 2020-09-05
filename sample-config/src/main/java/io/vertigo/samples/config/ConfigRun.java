534310ca0baf15c0a047f2c63372c26b7a220cee
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2020-09-05 15:03:11 +0200

    maven config ok

--- sample-config/src/main/java/io/vertigo/samples/config/ConfigRun.java
+++ sample-config/src/main/java/io/vertigo/samples/config/ConfigRun.java
@@ -1,10 +1,12 @@
 package io.vertigo.samples.config;
 
-import io.vertigo.app.AutoCloseableApp;
-import io.vertigo.app.config.NodeConfig;
-import io.vertigo.app.config.ModuleConfig;
 import io.vertigo.commons.CommonsFeatures;
+import io.vertigo.connectors.javalin.JavalinFeatures;
+import io.vertigo.core.node.AutoCloseableNode;
+import io.vertigo.core.node.config.ModuleConfig;
+import io.vertigo.core.node.config.NodeConfig;
 import io.vertigo.core.param.Param;
+import io.vertigo.datamodel.DataModelFeatures;
 import io.vertigo.vega.VegaFeatures;
 
 /***
@@ -21,15 +23,17 @@ public class ConfigRun {
 
 	public static void main(final String[] args) {
 		final NodeConfig nodeConfig = NodeConfig.builder()
+				.addModule(new JavalinFeatures().withEmbeddedServer(Param.of("port", "8080")).build())
 				.addModule(new CommonsFeatures().build())
-				.addModule(new VegaFeatures().withWebServices().withWebServicesEmbeddedServer(Param.of("port", "8080")).build())
+				.addModule(new DataModelFeatures().build())
+				.addModule(new VegaFeatures().withWebServices().withJavalinWebServerPlugin().build())
 				//-----Declaration of a module named 'Hello' which contains a webservice component.
 				.addModule(ModuleConfig.builder("Hello")
 						.addComponent(HelloWebServices.class)
 						.build())
 				.build();
 
-		try (AutoCloseableApp app = new AutoCloseableApp(nodeConfig)) {
+		try (AutoCloseableNode node = new AutoCloseableNode(nodeConfig)) {
 			//do whatever you want
 		}
 	}
