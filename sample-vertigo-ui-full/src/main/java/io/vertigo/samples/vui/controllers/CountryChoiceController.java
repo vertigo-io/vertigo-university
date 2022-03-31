2527d5f6d739650f76c2376bc1aff5f021cf0840
Piedeloup Nicolas <npi2loup@gmail.com>
2022-03-31 12:26:21 +0200

    [Ui] Add level5

---
+++ sample-vertigo-ui-full/src/main/java/io/vertigo/samples/vui/controllers/CountryChoiceController.java
@@ -0,0 +1,65 @@
+/**
+ * vertigo - simple java starter
+ *
+ * Copyright (C) 2013-2018, KleeGroup, direction.technique@kleegroup.com (http://www.kleegroup.com)
+ * KleeGroup, Centre d'affaire la Boursidiere - BP 159 - 92357 Le Plessis Robinson Cedex - France
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.vertigo.samples.vui.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.vertigo.samples.support.SampleUserSession;
+import io.vertigo.samples.vui.domain.Country;
+import io.vertigo.samples.vui.domain.User;
+import io.vertigo.ui.core.ViewContext;
+import io.vertigo.ui.core.ViewContextKey;
+import io.vertigo.ui.impl.springmvc.argumentresolvers.ViewAttribute;
+import io.vertigo.ui.impl.springmvc.controller.AbstractVSpringMvcController;
+
+@Controller
+@RequestMapping("/country")
+@ControllerAdvice(assignableTypes = { AbstractVSpringMvcController.class })
+public final class CountryChoiceController extends AbstractVSpringMvcController {
+	private static final ViewContextKey<User> userKey = ViewContextKey.of("user");
+	private static final ViewContextKey<Country> countriesKey = ViewContextKey.of("countriesMdl");
+
+	@ModelAttribute
+	public void initContext(final ViewContext viewContext, final SampleUserSession session) {
+		viewContext.publishDto(userKey, obtainUser(session));
+		viewContext.publishMdl(countriesKey, Country.class, null);
+	}
+
+	@PostMapping("/_changeCountry")
+	public ViewContext doChange(final ViewContext viewContext, @ViewAttribute("user") final User user, final SampleUserSession session) {
+		final User sessionUser = obtainUser(session);
+		sessionUser.setCouId(user.getCouId());
+		viewContext.publishDto(userKey, sessionUser);
+		return viewContext;
+	}
+
+	private User obtainUser(final SampleUserSession session) {
+		User user = session.getAttribute("sessionUser");
+		if (user != null) {
+			return user;
+		}
+		user = new User();
+		user.setCouId(1128L);
+		return user;
+	}
+}
