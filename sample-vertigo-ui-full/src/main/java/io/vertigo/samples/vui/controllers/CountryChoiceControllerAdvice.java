f87ba95f371de6249d426dba797ccdec650eb714
Piedeloup Nicolas <npi2loup@gmail.com>
2022-04-01 18:21:09 +0200

    [Ui] Split Controller and ControllerAdvice

--- sample-vertigo-ui-full/src/main/java/io/vertigo/samples/vui/controllers/CountryChoiceControllerAdvice.java
+++ sample-vertigo-ui-full/src/main/java/io/vertigo/samples/vui/controllers/CountryChoiceControllerAdvice.java
@@ -18,40 +18,41 @@
  */
 package io.vertigo.samples.vui.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import io.vertigo.account.authorization.VSecurityException;
 import io.vertigo.account.security.VSecurityManager;
-import io.vertigo.core.lang.Assertion;
 import io.vertigo.core.locale.MessageText;
 import io.vertigo.core.node.Node;
 import io.vertigo.samples.support.SampleUserSession;
+import io.vertigo.samples.vui.domain.Country;
 import io.vertigo.samples.vui.domain.User;
 import io.vertigo.ui.core.ViewContext;
 import io.vertigo.ui.core.ViewContextKey;
-import io.vertigo.ui.impl.springmvc.argumentresolvers.ViewAttribute;
 import io.vertigo.ui.impl.springmvc.controller.AbstractVSpringMvcController;
 
-@Controller
-@RequestMapping("/country")
-public final class CountryChoiceController extends AbstractVSpringMvcController {
+@ControllerAdvice(assignableTypes = { AbstractVSpringMvcController.class })
+public final class CountryChoiceControllerAdvice {
 	private static final ViewContextKey<User> userKey = ViewContextKey.of("user");
+	private static final ViewContextKey<Country> countriesKey = ViewContextKey.of("countriesMdl");
 
-	@PostMapping("/_changeCountry")
-	public ViewContext doChange(final ViewContext viewContext, @ViewAttribute("user") final User user) {
-		final User sessionUser = obtainUser();
-		sessionUser.setCouId(user.getCouId());
-		viewContext.publishDto(userKey, sessionUser);
-		return viewContext;
+	@ModelAttribute
+	public void initContext(final ViewContext viewContext) {
+		viewContext.publishDto(userKey, obtainUser());
+		viewContext.publishMdl(countriesKey, Country.class, null);
 	}
 
 	private User obtainUser() {
 		final SampleUserSession session = Node.getNode().getComponentSpace().resolve(VSecurityManager.class)
 				.<SampleUserSession> getCurrentUserSession().orElseThrow(() -> new VSecurityException(MessageText.of("No active session found")));
-		final User user = session.getAttribute("sessionUser");
-		Assertion.check().isNotNull(user);
+		User user = session.getAttribute("sessionUser");
+		if (user != null) {
+			return user;
+		}
+		user = new User();
+		user.setCouId(1128L);
+		session.putAttribute("sessionUser", user);
 		return user;
 	}
 }
