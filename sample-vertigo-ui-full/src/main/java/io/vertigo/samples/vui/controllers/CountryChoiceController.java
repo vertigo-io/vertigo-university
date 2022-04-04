b4f5927ed4a6a62af307756325a842e3053cc1b1
Piedeloup Nicolas <npi2loup@gmail.com>
2022-04-04 17:11:44 +0200

    [Ui] Use only one controller for choose contry

--- sample-vertigo-ui-full/src/main/java/io/vertigo/samples/vui/controllers/CountryChoiceController.java
+++ sample-vertigo-ui-full/src/main/java/io/vertigo/samples/vui/controllers/CountryChoiceController.java
@@ -19,15 +19,17 @@
 package io.vertigo.samples.vui.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.vertigo.account.authorization.VSecurityException;
 import io.vertigo.account.security.VSecurityManager;
-import io.vertigo.core.lang.Assertion;
 import io.vertigo.core.locale.MessageText;
 import io.vertigo.core.node.Node;
 import io.vertigo.samples.support.SampleUserSession;
+import io.vertigo.samples.vui.domain.Country;
 import io.vertigo.samples.vui.domain.User;
 import io.vertigo.ui.core.ViewContext;
 import io.vertigo.ui.core.ViewContextKey;
@@ -36,8 +38,18 @@ import io.vertigo.ui.impl.springmvc.controller.AbstractVSpringMvcController;
 
 @Controller
 @RequestMapping("/country")
+@ControllerAdvice(assignableTypes = { AbstractVSpringMvcController.class })
 public final class CountryChoiceController extends AbstractVSpringMvcController {
 	private static final ViewContextKey<User> userKey = ViewContextKey.of("user");
+	private static final ViewContextKey<Country> countriesKey = ViewContextKey.of("countriesMdl");
+
+	@ModelAttribute
+	public void initContext(final ViewContext viewContext) {
+		if (isNewContext()) { //InitContext only once
+			viewContext.publishDto(userKey, obtainUser());
+			viewContext.publishMdl(countriesKey, Country.class, null);
+		}
+	}
 
 	@PostMapping("/_changeCountry")
 	public ViewContext doChange(final ViewContext viewContext, @ViewAttribute("user") final User user) {
@@ -47,11 +59,24 @@ public final class CountryChoiceController extends AbstractVSpringMvcController 
 		return viewContext;
 	}
 
+	//	private User obtainUser() {
+	//		final SampleUserSession session = Node.getNode().getComponentSpace().resolve(VSecurityManager.class)
+	//				.<SampleUserSession> getCurrentUserSession().orElseThrow(() -> new VSecurityException(MessageText.of("No active session found")));
+	//		final User user = session.getAttribute("sessionUser");
+	//		Assertion.check().isNotNull(user);
+	//		return user;
+	//	}
 	private User obtainUser() {
 		final SampleUserSession session = Node.getNode().getComponentSpace().resolve(VSecurityManager.class)
 				.<SampleUserSession> getCurrentUserSession().orElseThrow(() -> new VSecurityException(MessageText.of("No active session found")));
-		final User user = session.getAttribute("sessionUser");
-		Assertion.check().isNotNull(user);
+		User user = session.getAttribute("sessionUser");
+		if (user != null) {
+			return user;
+		}
+		user = new User();
+		user.setCouId(1128L);
+		session.putAttribute("sessionUser", user);
 		return user;
 	}
+
 }
